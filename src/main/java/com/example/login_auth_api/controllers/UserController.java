bbca20671e273786c67b314d71b86fbb27def491
JoaoRViana <joaorviana98@gmail.com>
2024-09-11 15:37:19 -0300

    criando rota de delete

--- src/main/java/com/example/login_auth_api/controllers/UserController.java
+++ src/main/java/com/example/login_auth_api/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.example.login_auth_api.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,13 @@ public class UserController {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<String> deleteUser(@PathVariable String email) {
+        user user = this.repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        repository.delete(user);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
+++ src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/user").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/user").permitAll()                 
+                        .requestMatchers(HttpMethod.DELETE, "/user/{email}").permitAll()                
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // Permitir OPTIONS para todas as rotas
                         .anyRequest().authenticated()
                 )
